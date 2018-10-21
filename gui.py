2f180040566a60c54023c2a58efa532bc5ebc23e
Joschua Kraus <joschuakraus@JoschuasMacBookPro.local>
2018-10-21 13:53:19 +0200

    Final

--- gui.py
+++ gui.py
@@ -6,13 +6,36 @@ import threading
 import queue
 import socket
 from tkinter import messagebox
+import datetime
+
+class myCFred(threading.Thread): #searches acc and coo
+    def __init__(self, coordinates_Queue):
+        threading.Thread.__init__(self)
+        self.stoprequest = threading.Event()
+        self.coordinates_Queue = coordinates_Queue
+
+    def run(self):
+        UDP_IP = "10.0.24.199"
+        Port = 5006
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, Port))
+
+        while 1:
+            data, addr = sock.recvfrom(1024)
+            data = data.decode("utf-8") #EXTRACT INFORMATION TODO
+            self.coordinates_Queue.put(data)
+
+    def join(self, timeout=None):
+        self.stoprequest.set()
+        super(myCFred, self).join(timeout)
+
+
 
 class myFred (threading.Thread):
-    def __init__ (self, msg_Queue, coordinates_Queue, data_Queue):
+    def __init__ (self, msg_Queue, data_Queue):
         threading.Thread.__init__(self)
-        self.msg_Queue = msg_Queue
-        self.coordinates_Queue = coordinates_Queue
         self.data_Queue = data_Queue
+        self.msg_Queue = msg_Queue
         self.stoprequest = threading.Event()
 
     def run(self):
@@ -21,34 +44,27 @@ class myFred (threading.Thread):
   
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((UDP_IP, Port))
-        #gps_data_x = ---
-        #gps_data_y = --
 
         while 1:
-            #if gps_update_available:
-            # update gps_data_x, gps_data_y
             data, addr = sock.recvfrom(1024)
             data = data.decode("utf-8")
+
             if data.startswith("PACKAGE"):
                 self.msg_Queue.put(1)
-                #print gps_data_x
-            elif data.startswith("ACCELERATION"): #
-                #print gps_data_x
+            elif data.startswith("ACCELERATION"):
                 self.msg_Queue.put(2)
             else:
                 self.data_Queue.put(data)
-
 
     def join(self, timeout=None):
         self.stoprequest.set()
         super(myFred, self).join(timeout)
 
+
+
 def main():
     root = Tk()
     root.title("SenseTracker")
-    data = ""
-    coo = (-1,-1)
-    corrupt_flag = False
 
     t = Label(root, text = "")
     b1 = Label(root, text = "")
@@ -57,7 +73,7 @@ def main():
     w.config(font=("Arial", 44)) 
     t.config(font=("Arial", 44)) 
     b1.config(font=("Arial", 20)) 
-    b2.config(font=("Arial", 80)) 
+    b2.config(font=("Arial", 20)) 
     t.config(font=("Arial", 44)) 
     w.config(width=30)
     t.pack()
@@ -72,56 +88,62 @@ def main():
 
     #create queue for the thread
     msg_Queue = queue.Queue()
-    coordinates_Queue = queue.Queue()
     data_Queue = queue.Queue()
-    #create thread that monitors the udp package intake
-    t1 = myFred(msg_Queue, coordinates_Queue, data_Queue)
+    t1 = myFred(msg_Queue, data_Queue)
     t1.start()
 
-    while 1: #gui loop
-        try:
-            val = msg_Queue.get(False)
-            try:
-                coo = coordinates_Queue.get(False)
-            except queue.Empty:
-                pass
-            if val ==1:
+    coordinates_Queue = queue.Queue()
+    t2 = myCFred( coordinates_Queue)
+    t2.start()
+
+    data = " "
+    coo = "(-1, -1)"
+    corrupt_flag = False
+
+    while 1:
+        try: 
+            msg = msg_Queue.get(False) #is there an acc-interrupt? 
+            if msg == 1:
                 corrupt_flag = True
-                w.config(text= "PACKAGE INTERRUPT", font=("Arial", 44)) 
-                b1.config(text = "Long: "+ str(coo[0]) + "          Lat: " + str(coo[1])) #add time TODO
+                w.config(text= "PACKAGE OPENED", font=("Arial", 44)) 
                 root.update()
-                messagebox.showerror("Package INTERRUPT", \
+                messagebox.showerror("PACKAGE OPENED SIGNAL", \
                         "The package was opened. Please check the coordinates and track your parcel")
-                w.config(text= "Devices might be corrupted", font=("Arial", 44)) 
+                w.config(text= "Parcel was opened!", font=("Arial", 44)) 
+                b2.config(text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                 root.update()
-                with msg_Queue.mutex:
-                    msg_Queue.queue.clear()
-                with data_Queue.mutex:
-                    data_Queue.queue.clear()
-            if val == 2:
+            elif msg == 2:
+                #act
                 corrupt_flag = True
-                w.config(text= "ACCELERATION INTERRUPT", font=("Arial", 44)) 
-                b1.config(text = "Long: "+ str(coo[0]) + "          Lat: " + str(coo[1])) #add time TODO
+                w.config(text= "PACKAGE DROPED", font=("Arial", 44)) 
                 root.update()
-                messagebox.showerror("Acceleration INTERRUPT", \
+                messagebox.showerror("PACKAGE DROPED", \
                         "The package was droped. Please check the coordinates and track your parcel")
-                w.config(text= "Devices might be corrupted", font=("Arial", 44)) 
+                w.config(text= "Parcel was droped!", font=("Arial", 44)) 
+                b2.config(text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                 root.update()
-                with msg_Queue.mutex:
-                    msg_Queue.queue.clear()
-                with data_Queue.mutex:
-                    data_Queue.queue.clear()
-        except queue.Empty: #Queue empty = no interrupts
+            with data_Queue.mutex:
+                data_Queue.queue.clear()
+            with msg_Queue.mutex:
+                msg_Queue.queue.clear()
+
+        except queue.Empty: #no there is no interrupt
             try:
-                data = data_Queue.get(False)
-                if corrupt_flag == True:
-                    b1.config(text = "Long: "+ str(coo[0]) + "          Lat: " + str(coo[1])) #add time TODO
-                    root.update()
+                data = str(data_Queue.get(False))
+            except queue.Empty: #no there is no opening interrupt and also no acc-interrupt
+                pass
+        status.config(text=data)
+        root.update()
+        print (data)
+
+        if corrupt_flag == True:
+            try: 
+                coo = str(coordinates_Queue.get(False))
             except queue.Empty:
                 pass
-            status.config(text=data)
+            b1.config(text = "Location: " + coo) 
             root.update()
-            #print(data + 30*" ", end="\r", flush = True)
+
 
 if __name__ == "__main__":
     main()
--- udpserver.py
+++ udpserver.py
@@ -4,30 +4,32 @@ import time
 
 def main():
 
-	UDP_IP = "10.0.24.199"
-	Port = 5005
+    UDP_IP = "10.0.24.199"
+    Port = 5006
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	sock.bind((UDP_IP, Port))
-	#gps_data_x = ---
-	#gps_data_y = -- 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, Port))
+    #gps_data_x = ---
+    #gps_data_y = -- 
 
-	while 1:
-		#if gps_update_available:
-		# update gps_data_x, gps_data_y
-		data, addr = sock.recvfrom(1024)
-		data = data.decode("utf-8")
-		if data.startswith("PACKAGE"):
-			print("PACKAGE OPENING INTERRUPT DETECTED" + 30*" " , \
-				end = "\r", flush = True)
-			#print gps_data_x
-		elif data.startswith("ACCELERATION"): #
-			print("ACCELERATION INTERRUPT DETECTED " + 30*" "  , \
-				end = "\r", flush = True)
-			#print gps_data_x
-			time.sleep(3)
-		else:
-			print (data + 30*" ", end="\r", flush = True)
+    while 1:
+            #if gps_update_available:
+            # update gps_data_x, gps_data_y
+            data, addr = sock.recvfrom(1024)
+            data = data.decode("utf-8")
+            print (data)
+            continue
+            if data.startswith("PACKAGE"):
+                    print("PACKAGE OPENING INTERRUPT DETECTED" + 30*" " , \
+                            end = "\r", flush = True)
+                    #print gps_data_x
+            elif data.startswith("ACCELERATION"): #
+                    print("ACCELERATION INTERRUPT DETECTED " + 30*" "  , \
+                            end = "\r", flush = True)
+                    #print gps_data_x
+                    time.sleep(3)
+            else:
+                    print (data + 30*" ", end="\r", flush = True)
 if __name__ == "__main__":
 	main()
 
